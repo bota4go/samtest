515eb697ae699fd52f6697c200c987562880e4d4
Sergii Kachan <Sergii.Kachan@insigniafinancial.com.au>
2022-08-16 13:12:51 +1000

    it works to deploy but no test

--- bota4go/bota4goLambda/handler.js
+++ bota4go/bota4goLambda/handler.js
@@ -1,5 +1,15 @@
-const moment = require('moment');
+// const moment = require('moment');
 
 exports.bota4goLambda = async(event) =>{
     console.log('bota4go Your function run')
+
+    // const message = moment().format();
+    const message = 'Privet bota'
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(message)
+    };
+
+    return response;
 }
